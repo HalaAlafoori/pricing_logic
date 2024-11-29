3818731ec2bfefdda10a7334f1ee67594e7a93d5
The Voice <57120898+momardia54@users.noreply.github.com>
2024-11-29 05:33:59 -0500

    Update and rename setup_pricing_logic.py to doctype_setup.py

--- pricing_logic/scripts/setup/doctype_setup.py
+++ pricing_logic/scripts/setup/doctype_setup.py
@@ -21,11 +21,15 @@ def create_doctype_from_json(file_path):
         print(f"Error while creating Doctype from {file_path}: {e}")
 
 def execute():
-    # Get the path to the JSON files
+    # Get the path to the current project directory
     project_directory = os.path.dirname(os.path.abspath(__file__))
-    pricing_logic_json = os.path.join(project_directory, 'pricing_logic.json')
-    pricing_tier_json = os.path.join(project_directory, 'pricing_tier.json')
+    
+    # Define the path to the JSON files inside the doctype_definitions directory
+    pricing_logic_json = os.path.join(project_directory, 'pricing_logic', 'doctype_definitions', 'pricing_logic.json')
+    pricing_tier_json = os.path.join(project_directory, 'pricing_logic', 'doctype_definitions', 'pricing_tier.json')
+    tiered_pricing_logic_json = os.path.join(project_directory, 'pricing_logic', 'doctype_definitions', 'tiered_pricing_logic.json')
 
     # Create Doctypes from the JSON files
     create_doctype_from_json(pricing_logic_json)
     create_doctype_from_json(pricing_tier_json)
+    create_doctype_from_json(tiered_pricing_logic_json)
