ae96574494203204bf3e447491be88ab2d914e46
The Voice <57120898+momardia54@users.noreply.github.com>
2024-11-30 01:15:42 -0500

    Update hooks.py

--- pricing_logic/hooks.py
+++ pricing_logic/hooks.py
@@ -8,9 +8,9 @@ app_license = "agpl-3.0"
 # Apps
 # ------------------
 
-# daily task to set expired coupons code to inactive
+# daily task to set expired discounts code to inactive
 scheduler_events = {
     "daily": [
-        "pricing_logic.scripts.pricing_logic_scripts.tasks.deactivate_expired_coupons"
+        "pricing_logic.scripts.pricing_logic_scripts.tasks.deactivate_expired_discounts"
     ]
 }
