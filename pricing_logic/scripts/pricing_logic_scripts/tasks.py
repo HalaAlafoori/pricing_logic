150d53776d0fe02adb422ced5e88838f2a58a0cd
The Voice <57120898+momardia54@users.noreply.github.com>
2024-11-30 01:14:55 -0500

    Update tasks.py

--- pricing_logic/scripts/pricing_logic_scripts/tasks.py
+++ pricing_logic/scripts/pricing_logic_scripts/tasks.py
@@ -3,21 +3,21 @@
 import frappe
 from frappe.utils import nowdate
 
-# this task run daily to desactivate expired coupon codes
-def deactivate_expired_coupons():
+# this task run daily to desactivate expired discount codes
+def deactivate_expired_discounts():
     # Get today's date
     today = nowdate()
 
     # Find all active coupons with expired end_date
-    expired_coupons = frappe.get_all("Coupon Code", filters={
+    expired_discounts = frappe.get_all("Discount Code", filters={
         "active": 1,
         "end_date": ["<", today]
     })
 
-    for coupon in expired_coupons:
-        coupon_doc = frappe.get_doc("Coupon Code", coupon.name)
-        coupon_doc.active = 0  # Deactivate the coupon
-        coupon_doc.save()
+    for discount in expired_discounts:
+        discount_doc = frappe.get_doc("Discount Code", discount.name)
+        discount_doc.active = 0  # Deactivate the coupon
+        discount_doc.save()
         frappe.db.commit()  # Commit the changes
-        frappe.log("Deactivated expired coupon", coupon.name)
+        frappe.log("Deactivated expired discount", discount.name)
 
